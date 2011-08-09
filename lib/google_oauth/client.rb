b038d0da229036d3d5096a73f05ee41b98982d18
Tim Herd <erauqssidlroweht@eqdw.net>
2011-08-09 16:46:13 -0500

    more fixing broken library

--- lib/google_oauth/client.rb
+++ lib/google_oauth/client.rb
@@ -24,7 +24,7 @@ module GoogleOAuth
     end
 
     def authorize(options = {})
-      @access_token ||= consumer.get_access_token(
+      @access_token ||= consumer.get_token(
         options[:code],
         :redirect_uri => options[:callback] || @callback
       )
