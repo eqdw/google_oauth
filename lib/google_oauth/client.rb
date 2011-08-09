e5913e625e55364609e5dd1d08baaa62c76af474
Tim Herd <erauqssidlroweht@eqdw.net>
2011-08-09 15:45:36 -0500

    test

--- lib/google_oauth/client.rb
+++ lib/google_oauth/client.rb
@@ -5,22 +5,22 @@ require 'google_oauth/calendar'
 
 module GoogleOAuth
   class Client
-    
+
     def initialize(options = {})
       @application_id = options[:client_id]
       @application_secret = options[:client_secret]
       @callback = options[:redirect]
       @token = options[:token]
     end
-  
+
     def authorize_url(options = {})
       options[:scope] ||= 'https://www.google.com/m8/feeds/'
-      consumer.web_server.authorize_url(
+      consumer.authorize_url(
         :redirect_uri => options[:callback] || @callback,
         :scope => options[:scope]
       )
     end
-    
+
     def authorize(options = {})
       @access_token ||= consumer.web_server.get_access_token(
         options[:code],
@@ -29,13 +29,13 @@ module GoogleOAuth
       @token = @access_token.token
       @access_token
     end
-    
+
     private
       def consumer
         @consumer ||= OAuth2::Client.new(
           @application_id,
           @application_secret,
-          { 
+          {
             :site => "https://accounts.google.com",
             :authorize_url => '/o/oauth2/auth',
             :access_token_url => '/o/oauth2/token'
@@ -46,19 +46,19 @@ module GoogleOAuth
       def access_token
         OAuth2::AccessToken.new(consumer, @token)
       end
-      
+
       def _get_jsonc(url, params={})
         params.merge! 'alt' => 'jsonc'
         res = _get(url, params)
         GoogleOAuth::HashResponse.new(JSON.parse(res)) rescue res
       end
-      
+
       def _get_json(url, params={})
         params.merge! 'alt' => 'json'
         res = _get(url, params)
         JSON.parse(res) rescue res
       end
-      
+
       def _get(url, params={})
         oauth_response = access_token.get(url, params)
       end
@@ -74,4 +74,4 @@ module GoogleOAuth
       end
   end
 end
-   
+
